ffbef4433b772aae5a8b1db92231d6dfb6f52e10
Dominic <dom.benatar@gmail.com>
2023-09-26 11:51:29 +0100

    all stores added

--- app.js
+++ app.js
@@ -36,8 +36,88 @@ const seattle = {
   },
 };
 
+const tokyo = {
+  location: "tokyo",
+  minCust: 3,
+  maxCust: 24,
+  avgCookPerCust: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
+};
+
+const dubai = {
+  location: "dubai",
+  minCust: 11,
+  maxCust: 38,
+  avgCookPerCust: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
+};
+
+const paris = {
+  location: "paris",
+  minCust: 20,
+  maxCust: 38,
+  avgCookPerCust: 2.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
+};
+
+const lima = {
+  location: "lima",
+  minCust: 2,
+  maxCust: 16,
+  avgCookPerCust: 4.6,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
+};
+
 seattle.calculateSales();
 console.log(seattle);
+
+tokyo.calculateSales();
+console.log(tokyo);
+
+dubai.calculateSales();
+console.log(dubai);
+
+paris.calculateSales();
+console.log(paris);
+
+lima.calculateSales();
+console.log(lima);
 
 //const test = randomNumber(seattle.minCust, seattle.maxCust);
 //this.custmersPerHour.push(randomNumber);
@@ -46,9 +126,7 @@ console.log(seattle);
 //const cookieSales = document.getElementById("cookieSales");
 
 //const div = document.createElement("div");
-//Store name h2 - seattle
-//const h2 = document.createElement("h2");
-//h2.textcontext = Seattle.name;
-//div.appendChild(h2);
 
-// min max customers. Random np. customers p/ hour.
+//const h2 = document.createElement("h2");
+//h2.textcontext = seattle.location;
+//div.appendChild(h2);
